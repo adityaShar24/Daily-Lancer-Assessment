825a8572adb8a8c43b6a202047a322a3cd7c8681
adityaShar24 <adityarajansharma1234@gmail.com>
2023-11-13 11:37:46 +0530

    Added login controller

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -1,8 +1,9 @@
 from flask import request , json , make_response
 from database.repositories.user_repository import User_Repository
-from utils.constants import HTTP_201_CREATED , USER_REGISTERED_SUCCESS_MESSAGE
+from utils.constants import HTTP_201_CREATED , HTTP_400_BAD_REQUEST , USER_REGISTERED_MESSAGE , INVALID_PASSWORD_MESSAGE , USER_LOGGEDIN_MESSAGE
+from flask_jwt_extended import create_access_token
 import bson.json_util as json_util
-
+import datetime
 
 def register():
     body = json.loads(request.data)
@@ -14,5 +15,19 @@ def register():
     
     json_version = json_util.dumps(saved_user)
     
-    return make_response({"message": USER_REGISTERED_SUCCESS_MESSAGE, "user":json_version}, HTTP_201_CREATED)
+    return make_response({'message': USER_REGISTERED_MESSAGE.format(username), 'user': json_version} , HTTP_201_CREATED)
+
+def login():
+    body = json.loads(request.data)
     
+    username = body['username']
+    password = body['password']
+    
+    user = User_Repository().find_one({ "username":username })
+    
+    if password != user['password']:
+        return make_response({"message": INVALID_PASSWORD_MESSAGE} , HTTP_400_BAD_REQUEST)
+    
+    acess_token = create_access_token(identity=username , fresh=datetime.timedelta(hours=6))
+    
+    return make_response({"message": USER_LOGGEDIN_MESSAGE.format(username) , "access_token":acess_token} , HTTP_201_CREATED)
--- src/utils/constants.py
+++ src/utils/constants.py
@@ -4,4 +4,6 @@ CONNECTION_FAILED = 'Failed to connect to MongoDB'
 HTTP_201_CREATED = 201
 HTTP_400_BAD_REQUEST = 400
 
-USER_REGISTERED_SUCCESS_MESSAGE = "User has been registered successfully"
+USER_REGISTERED_MESSAGE = 'user {username} has been registered successfully!'
+USER_LOGGEDIN_MESSAGE = 'user {username} has been loggedin successfully!'
+INVALID_PASSWORD_MESSAGE = "Invalid password, please provide correct password"
