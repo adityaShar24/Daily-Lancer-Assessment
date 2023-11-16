bf1ea49e6208f96052b6c81d40ec8e82b225e2d2
adityaShar24 <adityarajansharma1234@gmail.com>
2023-11-16 13:50:06 +0530

    Added list by job application wrapper

--- src/routes/jobs_router.py
+++ src/routes/jobs_router.py
@@ -1,15 +1,22 @@
 from flask import Blueprint
-from controllers.job_controller import create , apply
+from flask_jwt_extended import jwt_required
+from controllers.job_controller import create , apply , list_by_job_apllications
 
 jobs_bp = Blueprint('jobs_bp', __name__)
 
 @jobs_bp.post('/jobs/create')
+@jwt_required()
 def create_job_wrapper():
     return create()
 
 @jobs_bp.post('/jobs/apply')
+@jwt_required()
 def apply_job_wrapper():
     return apply()
 
-# /jobs/list_by_job - job_id
-# /jobs/mine - user_id
+@jobs_bp.get('/jobs/applications/job/<int:job_id>')
+@jwt_required()
+def list_by_job_apllications_wrapper(job_id):
+    return list_by_job_apllications(job_id)
+
+
